2f03abe09156884fe2e34e8e0dda9c94736ab2be
Prince Raj <princeraj05082001@gmail.com>
2024-09-01 20:27:01 +0000

    logical operation

---
+++ main.py
@@ -0,0 +1,9 @@
+is_magician = True
+is_expert = False
+
+if is_magician and is_expert:
+  print("You are a master magician")
+elif is_magician:
+  print("at least you'r getting there")
+else:
+  print("You need magic power")
