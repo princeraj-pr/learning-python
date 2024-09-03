cf1c95eb6d4257cff2666e08f946b00f0eead2f0
Prince Raj <devprinceraj07@gmail.com>
2024-09-03 21:36:09 +0530

    find duplicates using set comprehensions

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# Removing duplicate items from the list without using set
-
 some_list = ['a','b','c','b','d','m','n','n']
-print(f"Original list: {some_list}")
 
-position = 1
-index = 0
-for item in some_list:
-  index = position
-  while index < len(some_list):
-    if str(item) == str(some_list[index]):
-      some_list.pop(index)
-    index += 1 
-  position += 1
- 
-print(f"Final list: {some_list}")
+duplicates = list({item for item in some_list if some_list.count(item) > 1})
+
+print(duplicates)
