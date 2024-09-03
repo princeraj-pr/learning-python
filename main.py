1d087b81eb69362a71d0e5e5ffd5f480e9c9d046
Prince Raj <devprinceraj07@gmail.com>
2024-09-03 17:41:43 +0530

    check driving age for Tesla car

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# Removing duplicate items from the list without using set
+#1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. 
+# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
 
-some_list = ['a','b','c','b','d','m','n','n']
-print(f"Original list: {some_list}")
+def checkDriverAge():
+    age = input("What is your age?: ")
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!")
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
 
-position = 1
-index = 0
-for item in some_list:
-  index = position
-  while index < len(some_list):
-    if str(item) == str(some_list[index]):
-      some_list.pop(index)
-    index += 1 
-  position += 1
- 
-print(f"Final list: {some_list}")
+checkDriverAge()
+
+#2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if you enter:
+#checkDriverAge(92);
+#it returns "Powering On. Enjoy the ride!"
+#also make it so that the default age is set to 0 if no argument is given.
+
+def checkDriverAge(age=0):
+    if int(age) < 18:
+        print("Sorry, you are too yound to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!")
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+
+checkDriverAge(92)
