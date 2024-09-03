b5de8f2ffc24ebc362131a4f3d2396bcc3bd1db4
Prince Raj <devprinceraj07@gmail.com>
2024-09-03 17:11:51 +0530

    find duplicates

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# Removing duplicate items from the list without using set
-
 some_list = ['a','b','c','b','d','m','n','n']
-print(f"Original list: {some_list}")
 
-position = 1
-index = 0
+duplicate_list = []
+
 for item in some_list:
-  index = position
-  while index < len(some_list):
-    if str(item) == str(some_list[index]):
-      some_list.pop(index)
-    index += 1 
-  position += 1
- 
-print(f"Final list: {some_list}")
+    if some_list.count(item) > 1:
+        if item not in duplicate_list:
+            duplicate_list.append(item)
+        
+print(duplicate_list)
